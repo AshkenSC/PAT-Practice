b83695ad4b1d216acae8cade9cf871054e90f096
ashken <393940378@qq.com>
2020-07-02 00:03:29 +0800

    LeetCode 1431
    在每轮循环重新给临时列表temp赋值为candies时，使用切片进行值传递，否则将进行地址传递，修改了candies的值

--- LeetCode/1431. Kids With the Greatest Number of Candies.py
+++ LeetCode/1431. Kids With the Greatest Number of Candies.py
@@ -7,7 +7,7 @@ class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         output = list()
         for i in range(len(candies)):
-            temp = candies
+            temp = candies[:]   # 这里增加[:]，使用值传递，否则是地址传递，会导致结果错误
             temp[i] += extraCandies
             if temp[i] == max(temp):
                 output.append("true")
@@ -18,7 +18,7 @@ class Solution:
 
 
 # test
-# candies = [2,3,5,1,3]
-# extra_candies = 3
-# solution = Solution()
-# print(solution.kidsWithCandies(candies, extra_candies))
+candies = [2,3,5,1,3]
+extra_candies = 3
+solution = Solution()
+print(solution.kidsWithCandies(candies, extra_candies))
