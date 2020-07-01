0cbd96c192c69bce7518011a7f4d28a0ebb70bdc
ashken <393940378@qq.com>
2020-07-01 23:58:26 +0800

    LeetCode 1431
    修正了输出格式错误。

--- LeetCode/1431. Kids With the Greatest Number of Candies.py
+++ LeetCode/1431. Kids With the Greatest Number of Candies.py
@@ -10,15 +10,15 @@ class Solution:
             temp = candies
             temp[i] += extraCandies
             if temp[i] == max(temp):
-                output.append(True)
+                output.append("true")
             else:
-                output.append(False)
+                output.append("false")
 
         return output
 
 
 # test
-candies = [2,3,5,1,3]
-extra_candies = 3
-solution = Solution()
-print(solution.kidsWithCandies(candies, extra_candies))
+# candies = [2,3,5,1,3]
+# extra_candies = 3
+# solution = Solution()
+# print(solution.kidsWithCandies(candies, extra_candies))
