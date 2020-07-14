efe63fad3590bfb1024eba8dedf56324d263ae9b
ashken <393940378@qq.com>
2020-07-14 22:47:34 +0800

    LeetCode 0021
    通过了0/122个测试点。
    
    用两个指针分别指向l1, l2，逐个比较元素，将小的放入output。最后其中一个链表剩下的部分再接到output上。

---
+++ LeetCode/0021. Merge Two Sorted Lists.py
@@ -0,0 +1,50 @@
+# 21. Merge Two Sorted Lists
+'''
+用两个指针分别指向l1, l2，逐个比较元素，将小的放入output。最后其中一个链表剩下的部分再接到output上
+'''
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+
+        # 确定合并链表的头结点
+        if l1.val < l2.val:
+            output = l1
+            index1 = l1.next
+            index2 = l2
+        else:
+            output = l2
+            index1 = l1
+            index2 = l2.next
+
+        # 遍历两个链表并比较、插入output
+        output_pointer = output
+        while index1 is not None and index2 is not None:
+            if index1.val < index2.val:
+                output_pointer.next = index1
+                index1 = index1.next
+            else:
+                output_pointer.next = index2
+                index2 = index2.next
+            output_pointer = output_pointer.next
+
+        # 将剩下的链表接到output上
+        if index1 is not None:
+            output_pointer.next = index1
+        else:
+            output_pointer.next = index2
+
+        return output
+
+# test
+# sol = Solution()
+# l1 = ListNode()
+# ans = sol.mergeTwoLists()
+# print(ans)
