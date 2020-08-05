533aadeed74c8ce16457ff13eb8043823e993825
ashken <393940378@qq.com>
2020-08-05 17:28:58 +0800

    LeetCode 0121. Best Time to Buy and Sell Stock
    Runtime:  40.95%%
    Memory Usage: 31.85%
    
    思路：动态规划。
    1）记录第i天之前买入的最小值;
    2）计算：如果在第i天之前的最小值买入，并在第i天卖出的利润，得到第i天的最大利润;
    3）对比每一天的最大利润，得到结果。

---
+++ LeetCode/!121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,26 @@
+/*
+0121. Best Time to Buy and Sell Stock
+
+给定一个某股票的价格数组，其中第i个元素就是该股票第i天的价格。
+如果最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算能获取的最大利润。
+
+思路：动态规划。
+1）记录第i天之前买入的最小值;
+2）计算：如果在第i天之前的最小值买入，并在第i天卖出的利润，得到第i天的最大利润;
+3）对比每一天的最大利润，得到结果。
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() <= 1)
+            return 0;
+        int minPrice = prices[0];
+        int maxProfitValue = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            maxProfitValue = max(maxProfitValue, prices[i] - minPrice);
+            minPrice = min(minPrice, prices[i]);
+        }
+        return maxProfitValue;
+    }
+};
