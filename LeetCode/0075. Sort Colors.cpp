38fb884b9dd9ad6870de191f9e0351554a572245
ashken <393940378@qq.com>
2020-09-25 10:46:51 +0800

    LeetCode 0075. Sort Colors
    思路2：使用三指针，一次遍历。
    三指针为：左指针left，右指针right，操作指针cur
    每次循环，先观察cur指向的值。
    如果cur=0,则将cur与left指向的值互换，然后left向右，cur向右；
    如果cur=1，只右移cur；
    如果cur=2，则将cur与right指向的值互换，然后**只左移动right**。
    **注意循环范围！！** cur < right

--- LeetCode/0075. Sort Colors.cpp
+++ LeetCode/0075. Sort Colors.cpp
@@ -5,13 +5,23 @@
 将他们就地排序，使得相同颜色的对象是相邻的，且颜色顺序为红，白，蓝。
 这里使用整数0，1，2表示颜色红，白，蓝。
 
-思路：使用冒泡排序。
+进阶：能否想出一个仅使用常数空间的一趟扫描算法？
+
+思路1：使用冒泡排序。
+
+思路2：使用三指针，一次遍历。
+三指针为：左指针left，右指针right，操作指针cur
+每次循环，先观察cur指向的值。
+如果cur=0,则将cur与left指向的值互换，然后left向右，cur向右；
+如果cur=1，只右移cur；
+如果cur=2，则将cur与right指向的值互换，然后**只左移动right**。
+**注意循环范围！！** cur < right
 */
 
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution0 {
 public:
     void sortColors(vector<int>& nums) {
         for (int i = 0; i < nums.size(); i++)
@@ -20,5 +30,20 @@ public:
                     swap(nums[j], nums[j + 1]);
             }
 
+    }
+};
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1, cur = 0;
+        while (cur < right) {
+            if (nums[cur] == 0) 
+                swap(nums[cur++], nums[left++]);
+            else if (nums[cur] == 1)
+                cur++;
+            else if (nums[cur] == 2)
+                swap(nums[cur], nums[right--]);
+        }
     }
 };
