bfebb9b7773c2df85b896eae1b5fea3b5539276f
ashken <393940378@qq.com>
2020-09-27 21:45:17 +0800

    0207. Course Schedule
    你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
    在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
    给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
    
    思路：参考官方解答

---
+++ LeetCode/!!0207. Course Schedule.cpp
@@ -0,0 +1,52 @@
+/*
+0207. Course Schedule
+
+你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
+在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
+给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
+
+思路：参考官方解答
+*/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+
+private:
+    vector<vector<int>> edges;
+    vector<int> visited;
+    bool valid = true;
+
+public:
+
+    void dfs(int u) {
+        visited[u] = 1;
+        for (int v : edges[u]) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid)
+                    return;
+            }
+            else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[u] = 2;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        edges.resize(numCourses);
+        visited.resize(numCourses);
+        for (const auto& info : prerequisites) {
+            edges[info[1]].push_back(info[0]);
+        }
+        for (int i = 0; i < numCourses && valid; ++i) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+        return valid;
+    }
+};
