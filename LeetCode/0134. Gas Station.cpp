29c206bffa55034329f09cfaf537b2af1f87395b
ashken <393940378@qq.com>
2020-11-02 11:26:32 +0800

    LeetCode 0134. Gas Station
    补充了思路2.

--- LeetCode/0134. Gas Station.cpp
+++ LeetCode/0134. Gas Station.cpp
@@ -9,12 +9,17 @@
 遍历数组，先检查能否做起点，不能则跳过；
 能做起点的就测试能否走一圈，中间走不通则直接跳过，测试下一个。直到遍历完成。
 
+思路2：
+官方算法优化了下一次开始的位置，它不从n+1开始尝试，而是从上一次断掉的位置开始下一轮尝试。
+官方算法优化的依据是：[A,B,C,D]，如果以A为起点，不能从C到达D，那么以B或C为起点，也一定不能到达D。
+（不要看官方解答的分析，直接看代码。不然晕了。）
+
 */
 
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
 
     bool isValidStation(vector<int>& gas, vector<int>& cost, int i) {
@@ -45,5 +50,27 @@ public:
             }
         }
         return -1;
+    }
+};
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int len = gas.size();
+
+        int startPos = 0;
+        int currentGas = 0, totalGasDiff = 0;
+        for (int i = 0; i < len; i++) {
+            currentGas += gas[i] - cost[i];
+            totalGasDiff += gas[i] - cost[i];
+            if (currentGas < 0) {
+                startPos = i + 1;   // 直接从当前位下一个位置开始考察
+                currentGas = 0;
+            }
+        }
+        if (totalGasDiff < 0)
+            return -1;
+        else
+            return startPos;
     }
 };
