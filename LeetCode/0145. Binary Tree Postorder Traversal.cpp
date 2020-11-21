138182b616ea76dc02d4fb1711770ec8c76c3b58
ashken <393940378@qq.com>
2020-11-21 22:16:05 +0800

    LeetCode 0145. Binary Tree Postorder Traversal
    给定一个二叉树，返回它的 后序 遍历。

---
+++ LeetCode/0145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,52 @@
+/*
+0145. Binary Tree Postorder Traversal
+
+给定一个二叉树，返回它的 后序 遍历。
+
+*/
+
+#include <vector>
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode *> s;
+        vector<int> res;
+
+        if (root == nullptr)
+            return res;
+
+        s.emplace(root);
+        TreeNode *prev = nullptr;
+        while (s.empty() == false) {
+            while (root != nullptr) {
+                s.emplace(root);
+                root = root->left;
+            }
+            root = s.top();
+            s.pop();
+            if (root->right == nullptr || root->right == prev) {
+                res.push_back(root->val);
+                prev = root;
+                root = nullptr;
+            }
+            else {
+                s.emplace(root);
+                root = root->right;
+            }
+        }
+
+        return res;
+    }
+};
