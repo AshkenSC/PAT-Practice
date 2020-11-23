ee021beba2dee8a429c823142346f3ec2f44b32a
ashken <393940378@qq.com>
2020-11-23 16:57:36 +0800

    LeetCode 0094. Binary Tree Inorder Traversal
    更新了迭代写法。

--- LeetCode/0094. Binary Tree Inorder Traversal.cpp
+++ LeetCode/0094. Binary Tree Inorder Traversal.cpp
@@ -4,6 +4,19 @@
 实现二叉树的中序遍历。
 */
 
+#include <vector>
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     vector<int> result;
@@ -21,5 +34,29 @@ public:
             return result;
         traverse(root);
         return result;
+    }
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+
+        if (root == nullptr)
+            return res;
+        
+        stack<TreeNode *> s;
+        while (s.empty() == false || root != nullptr) {
+            while (root != nullptr) {
+                s.emplace(root);
+                root = root->left;
+            }
+            root = s.top();
+            s.pop();
+            res.push_back(root->val);
+            root = root->right;
+        }
+
+        return res;
     }
 };
