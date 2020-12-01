9f662956960ac7f176275d4362006f962e881231
ashken <393940378@qq.com>
2020-12-01 13:35:16 +0800

    LeetCode 0047. Permutations II
    补充注释。

--- LeetCode/!!0047. Permutations II.cpp
+++ LeetCode/!!0047. Permutations II.cpp
@@ -12,6 +12,18 @@
 后来，想到通过判断if (i > startIndex && nums[i] == nums[i - 1]）则直接continue，又走进另一个极端：完全没结果。
 然后看liweiwei参考答案。有了第三版成功版。
 https://leetcode-cn.com/problems/permutations-ii/solution/hui-su-suan-fa-python-dai-ma-java-dai-ma-by-liwe-2/
+
+为什么需要if (i > startIndex && nums[i] == nums[i - 1] && used[i - 1] == false)
+其中的used[i - 1] == false，意味着前一个元素不仅相同，而且还没使用，等待进行排列。
+试想，对于数列[1, 1', 2]，进行排列。
+
+首先是1做开头，[1, 1'2], [1, 21']，接下来1被重新设为used == false了，轮到1'做开头了。
+但由于1和1'完全相等，因此1'开头一定是得到一系列和1开头完全相同的排列。也就是说，要把1'开头的排列完全去除。
+这样，判断标准就是：nums[i] == nums[i - 1] && used[i - 1] == false
+
+那么，当nums[i] == nums[i - 1] && used[i - 1] == true，是什么情形呢？
+那就是以1开头，然后进入下一层递归，遍历到了1'，这时1已经被使用了，还在等待排列完成。
+这时不能结束循环，不然就无法得到完整的排列了。所以，不能单纯的用nums[i] == nums[i - 1]做跳过循环的条件。
 */
 
 #include <vector>
