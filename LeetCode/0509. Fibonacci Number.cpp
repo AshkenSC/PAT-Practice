761a8dc6fd8989479b92de4fa4ab521266e55226
ashken <393940378@qq.com>
2020-12-20 13:30:46 +0800

    LeetCode 0509. Fibonacci Number
    斐波那契数，通常用 F(n) 表示，形成的序列称为斐波那契数列。
    该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。
    
    思路：递归，递推公式。

---
+++ LeetCode/0509. Fibonacci Number.cpp
@@ -0,0 +1,20 @@
+/*
+0509. Fibonacci Number
+
+斐波那契数，通常用 F(n) 表示，形成的序列称为斐波那契数列。
+该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。
+
+思路：递归，递推公式。
+*/
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fib(n - 1) + fib(n - 2);
+    }
+};
