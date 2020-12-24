e4834b0c074ed9f858821497b9347c1d02d6b22b
ashken <393940378@qq.com>
2020-12-24 14:42:20 +0800

    LeetCode 0263. Ugly Number
    编写一个程序判断给定的数是否为丑数。
    丑数就是只包含质因数 2, 3, 5 的正整数。
    注：1是丑数。

--- Playground/main.py
+++ Playground/main.py
@@ -4,21 +4,17 @@ from typing import List
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        # dp = [[-prices[0], 0, 0]] + [[0, 0, 0]] * (n - 1)
-        # 不能这样写！这样写修改某一行某元素，其他行对应元素也会修改！！！
-        dp = [[-prices[0], 0, 0]] + [0] * (n - 1)
-        for i in range(1, n):
-            # 第i天手中持有股票
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i])
-            # 第i天处于冷冻期
-            dp[i][1]  = dp[i - 1][0] + prices[i]
-            # 第i天手中没有股票
-            dp[i][2] = max(dp[i - 1][1], dp[i - 1][2])
-        
-        return max(dp[n-1][1], dp[n-1][2])
-
+    def isUgly(self, num: int) -> bool:
+        while num % 2 == 0:
+            num /= 2
+        while num % 3 == 0:
+            num /= 3
+        while num % 5 == 0:
+            num /= 5
+        if num == 1:
+            return True
+        else:
+            return False
 
 
 sol = Solution()
