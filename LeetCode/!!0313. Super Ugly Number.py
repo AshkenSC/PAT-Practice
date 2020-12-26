2e452fc25eea4ce365849f773b68fbb4320c382e
ashken <393940378@qq.com>
2020-12-26 17:12:03 +0800

    LeetCode 0313. Super Ugly Number
    编写一段程序来查找第 n 个超级丑数。
    超级丑数是指其所有质因数都是长度为 k 的质数列表 primes 中的正整数。
    
    思路：模仿丑数2写法。

--- LeetCode/!!0313. Super Ugly Number.py
+++ LeetCode/!!0313. Super Ugly Number.py
@@ -1,12 +1,18 @@
-# 198
+'''
+0313. Super Ugly Number
 
-from typing import List
+编写一段程序来查找第 n 个超级丑数。
+超级丑数是指其所有质因数都是长度为 k 的质数列表 primes 中的正整数。
+
+思路：模仿丑数2写法。
+
+'''
 
 class Solution:
     def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
         ugly_set = [1]
         pointers = [0] * len(primes)
-        while len(ugly_set) < 1000:
+        while len(ugly_set) < n:
             candidates = [ugly_set[pointers[i]] * primes[i] for i in range(len(primes))]
             new_ugly = min(candidates)
             ugly_set.append(new_ugly)
@@ -15,8 +21,4 @@ class Solution:
                     pointers[i] += 1
         
         return ugly_set[n - 1]
-    
-
-sol = Solution()
-res = sol.nthSuperUglyNumber(12, [2,7,13,19])
-print(res)
+        
