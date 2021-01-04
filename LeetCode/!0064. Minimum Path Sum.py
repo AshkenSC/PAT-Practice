354d89da2f07abc0e5647d8237d9d72f395d4452
ashken <393940378@qq.com>
2021-01-04 12:23:13 +0800

    LeetCode 0064. Minimum Path Sum
    新增python解答。

--- LeetCode/!0064. Minimum Path Sum.py
+++ LeetCode/!0064. Minimum Path Sum.py
@@ -1,21 +1,12 @@
-# 198
 '''
-1) 起点
-dp[i][j] = dp[i][j]
+0064. Minimum Path Sum
 
-2) 只能从左边来
-dp[i][j] = dp[i][j - 1]
+给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+说明：每次只能向下或者向右移动一步。
 
-3) 只能从上边来
-dp[i][j] = dp[i - 1][j]
-
-4) 可以从上和左来
-dp[i][j] = min(dp[i][j - 1], dp[i - 1][j])
-
+思路：动态规划。
 
 '''
-
-from typing import List
 
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
@@ -34,10 +25,4 @@ class Solution:
                     grid[i][j] = min(grid[i][j - 1], grid[i - 1][j]) + grid[i][j]
         
         return grid[m - 1][n - 1]
-        
-
-
-
-sol = Solution()
-res = sol.coinChange([2], 3)
-print(res)
+   
