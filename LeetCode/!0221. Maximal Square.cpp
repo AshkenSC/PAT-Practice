023170742a8976f59c16ebcdf06f8af94d19ceb1
ashken <393940378@qq.com>
2021-01-06 13:32:54 +0800

    LeetCode 0221. Maximal Square
    在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
    
    思路：动态规划经典题，
    总体思路，参考官方题解。
    递推方程的推导证明，参考：https://leetcode-cn.com/problems/maximal-square/solution/zui-da-zheng-fang-xing-by-leetcode-solution/

--- LeetCode/!0221. Maximal Square.cpp
+++ LeetCode/!0221. Maximal Square.cpp
@@ -1,21 +1,11 @@
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-// 0322. Coin Change
 /*
-这题实际上给出的是一个直角三角形。只能向下或者向右走。
-1）在原点
-dp{i]{j] = dp[i][j]
-2）只能从上来
-dp[i][j] = dp[i - 1][j]
-3）只能从左上来
-dp[i][j] = dp[i - 1][j - 1]
-4）可从左上or上来
-dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1])
+0221. Maximal Square
 
+在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
 
+思路：动态规划经典题，
+总体思路，参考官方题解。
+递推方程的推导证明，参考：https://leetcode-cn.com/problems/maximal-square/solution/zui-da-zheng-fang-xing-by-leetcode-solution/
 */
 
 class Solution {
@@ -52,21 +42,3 @@ public:
         return maxSide * maxSide;
     }
 };
-
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-int main() {
-    Solution sol;
-    //vector<vector<int>> input = {{0,0,0},{0,1,0},{0,0,0}};
-    vector<vector<char>> input = {{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
-    int res = sol.maximalSquare(input);
-    cout << res;
-
-    return 0;
-}
