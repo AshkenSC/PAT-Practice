6bd1c8559f640db335bc88ee8eee1641998aabdc
ashken <393940378@qq.com>
2021-01-16 22:02:16 +0800

    LeetCode 0454. 4Sum II
    补充注释。

--- LeetCode/!0454. 4Sum II.cpp
+++ LeetCode/!0454. 4Sum II.cpp
@@ -2,6 +2,10 @@
 0454. 4Sum II
 
 思路：将数组分为两部分，问题转化为在AB中取两个数求和，在CD中找是否有两个数的和是其相反数。
+将数组分为两部分，问题转化为在AB中取两个数求和，在CD中找是否有两个数的和是其相反数。
+用一个map存储：在AB中各取一个数求和，这样的和一共有多少组合？
+然后在CD中，每次各取一个数，求和取反，在这字典里找其对应的value，将这个value加入res。
+如果在CD中找到的数，其相反数在字典里并不存在，就会直接返回0.
 
 参考：https://leetcode-cn.com/problems/4sum-ii/solution/tu-jie-fen-zu-ha-xi-si-lu-jian-dan-by-treasure-35/
 */
