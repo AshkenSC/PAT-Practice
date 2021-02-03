abff78b64870c3682cb2b587e40ff3a57d5109a9
ashken <393940378@qq.com>
2021-02-04 01:44:48 +0800

    LeetCode 0103. Binary Tree Zigzag Level Order Traversal
    新增解法：整体方法类似102题层序遍历。但如果是偶数层，将其逆序即可。

--- LeetCode/0103. Binary Tree Zigzag Level Order Traversal.cpp
+++ LeetCode/0103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -8,6 +8,9 @@
 普通层序遍历进入队列，这个尝试进入栈。
 但是，如果只是设计一个栈，那么每次出站一个元素后，直接插入其孩子节点，就会干扰到出站元素后面的兄弟节点正常出站。
 所以使用两个栈交替出入。
+
+思路2：
+整体方法类似102题层序遍历。但如果是偶数层，将其逆序即可。
 */
 
 #include <vector>
@@ -78,5 +81,46 @@ public:
         }
 
         return result;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+
+        if (root == NULL) {
+            return res;
+        }
+
+        int level = 1;  // 用于记录层数，判定节点存入次序
+        queue<TreeNode*> q;
+        q.emplace(root);
+        while (q.empty() == false && root != NULL) {
+            vector<int> subList;
+            int currentLevelSize = q.size();
+
+            for (int i = 0; i < currentLevelSize; ++i) {
+                auto node = q.front();
+                q.pop();
+                subList.push_back(node->val);
+
+                if (node->left) {
+                    q.emplace(node->left);
+                }
+                if (node->right) {
+                    q.emplace(node->right);
+                }
+            }
+            
+            // 如果是偶数层，将其逆序
+            if (level % 2 == 0) {
+                reverse(subList.begin(), subList.end());
+            }
+            ++level; 
+            res.push_back(subList);
+        }
+
+        return res;
     }
 };
