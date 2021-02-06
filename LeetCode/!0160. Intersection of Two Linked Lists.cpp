932908140f4519f52eaf5a4f6f78228b65d34b13
ashken <393940378@qq.com>
2021-02-06 11:29:16 +0800

    LeetCode 0160. Intersection of Two Linked Lists
    重做本题。

--- LeetCode/!0160. Intersection of Two Linked Lists.cpp
+++ LeetCode/!0160. Intersection of Two Linked Lists.cpp
@@ -6,30 +6,36 @@
 让两个指针分别从A，B头开始向后遍历，如果到了末尾，就从另一个链表头开始继续遍历，直到两个指针相遇为止。
 这是因为，假设链表A自有部分长a，链表B自有部分长b，公共部分长c，那么这种情况下，两个指针都会走a+b+c的长度。
 因此，如果他们存在公共部分，一定是在公共点起始处相遇。
+
+参考:
+https://leetcode-cn.com/problems/intersection-of-two-linked-lists/solution/jiao-ni-yong-lang-man-de-fang-shi-zhao-dao-liang-2/
 */
 
 class Solution {
 public:
-    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-        if (headA == nullptr || headB == nullptr) {
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (!headA || !headB) {
             return nullptr;
         }
-        else {
-            ListNode* pointerA = headA;
-            ListNode* pointerB = headB;
 
-            while (pointerA != pointerB) {
-                if (pointerA != nullptr)
-                    pointerA = pointerA->next;
-                else
-                    pointerA = headB;
-                if (pointerB != nullptr)
-                    pointerB = pointerB->next;
-                else
-                    pointerB = headA;
+        ListNode *pA = headA;
+        ListNode *pB = headB;
+        while (pA != pB) {
+            if (pA != nullptr) {
+                pA = pA->next;
+            }
+            else {
+                pA = headB;
             }
 
-            return pointerA;
+            if (pB != nullptr) {
+                pB = pB->next;
+            }
+            else {
+                pB = headA;
+            }
         }
+
+        return pA;
     }
 };
