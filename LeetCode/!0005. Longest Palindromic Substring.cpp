3ce586c1f29767f7384b8925cdceded3823862df
ashken <393940378@qq.com>
2021-02-14 12:01:06 +0800

    LeetCode 0005. Longest Palindromic Substring
    重做本题。

--- LeetCode/!0005. Longest Palindromic Substring.cpp
+++ LeetCode/!0005. Longest Palindromic Substring.cpp
@@ -13,24 +13,31 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
+        // dp[i][j] 从i开头到j结尾的字符串是否是回文串
         vector<vector<int>> dp(n, vector<int>(n));
         string res;
-        // l -- 检查的字符串长度
+        // l -- 被查字符串头尾之间的距离差
         // i -- 字符串起始位置
         // j -- 字符串终结位置
         for (int l = 0; l < n; l++) {
             for (int i = 0; i + l < n; i++) {
                 int j = i + l;
+                // 如果头尾之间距离差为0（总长度为1），则一定是回文串
                 if (l == 0) {
                     dp[i][j] = 1;
                 }
+                // 如果头尾之间距离差为1（总长度为2），只要头尾相同则是回文串
                 else if (l == 1) {
                     dp[i][j] = (dp[i] == dp[j]);
                 }
+                // 否则，[i,j]是否为回文串取决于：
+                // 1）头尾是否相等
+                // 2）[i+1, j-1]是否为回文串
                 else {
                     dp[i][j] = (dp[i] == dp[j] && dp[i + 1][j - 1]);
                 }
 
+                // 如果[i,j]为回文串，且比现有最长回文串更长，则更新记录
                 if (l + 1 > res.size() && dp[i] == dp[j]) {
                     res = s.substr(i, l + 1);
                 }
