3b288d12660a9cdc5ec61e3003bd8a2826cf0e77
ashken <393940378@qq.com>
2021-02-16 00:39:29 +0800

    LeetCode 0056. Merge Intervals
    重做本题。

--- LeetCode/0056. Merge Intervals.cpp
+++ LeetCode/0056. Merge Intervals.cpp
@@ -25,23 +25,29 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> res;
-        if (intervals.size() == 0)
+        if (intervals.size() == 0) {
             return res;
+        }
 
         // 将区间按左端点进行升序排序
-        sort(intervals.begin(), intervals.end(), cmp);
+        // 使用lambda表达式
+        sort(intervals.begin(), intervals.end(),
+        [](vector<int>& a, vector<int>& b){return a[0] < b[0];});
+
         // 遍历区间
-        res.emplace_back(intervals[0]);
-        for (int i = 1; i < intervals.size(); i++) {
+        res.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); ++i) {
             // 如果当前遍历到的区间的左端点 > 结果集合中最后一个区间的右端点，则两个没有交集：
             // 将当前区间插入结果集合
-            if (intervals[i][0] > res.back()[1])
-                res.emplace_back(intervals[i]);
+            if (intervals[i][0] > res.back()[1]) {
+                res.push_back(intervals[i]);
+            }
             // 如果当前遍历到的区间的左端点 <= 结果集合中最后一个区间的右端点，则两个有交集：
             // 将更新结果集合中最后一个区间的右端点。
             // 取最后一个区间的右端点和当前区间右端点中较大的那个。
-            else
+            else {
                 res.back()[1] = max(res.back()[1], intervals[i][1]);
+            }
         }
 
         return res;
