0ebd74840c6b1dbb4f67bafddd5abe18e08b7286
ashken <393940378@qq.com>
2021-02-16 16:36:00 +0800

    LeetCode 0002. Add Two Numbers
    采用一刷参考答案的思想去计算。在计算长度不同的部分时，代码繁琐了。

--- LeetCode/!0002. Add Two Numbers.cpp
+++ LeetCode/!0002. Add Two Numbers.cpp
@@ -11,6 +11,79 @@ https://leetcode-cn.com/problems/add-two-numbers/solution/liang-shu-xiang-jia-by
 当数的长度不一样时，不补全长度，而是直接逐位计算和，并考虑进位。
 */
 
+
+// 二刷
+// 采用上面的思想去计算。在计算长度不同的部分时，代码繁琐了。
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode();
+        ListNode *p = head;
+        int carry = 0;
+        
+        // 计算公共部分
+        while (l1 && l2) {
+            int current = l1->val + l2->val + carry;
+            if (current >= 10) {
+                current -= 10;
+                carry = 1;
+            }
+            else {
+                carry = 0;
+            }
+
+            p->next = new ListNode(current);
+            p = p->next;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+
+        // 计算非公共部分
+        // 这一部分l1和l2代码冗余
+        if (l1) {
+            while (l1) {
+                int current = l1->val + carry;
+                if (current >= 10) {
+                    current -= 10;
+                    carry = 1;
+                }
+                else {
+                    carry = 0;
+                }
+
+                p->next = new ListNode(current);
+                p = p->next;
+                l1 = l1->next;
+            }
+            if (carry == 1) {
+                p->next = new ListNode(1);
+            }
+        }
+        else {
+            while (l2) {
+                int current = l2->val + carry;
+                if (current >= 10) {
+                    current -= 10;
+                    carry = 1;
+                }
+                else {
+                    carry = 0;
+                }
+
+                p->next = new ListNode(current);
+                p = p->next;
+                l2 = l2->next;
+            }
+            if (carry == 1) {
+                p->next = new ListNode(1);
+            }
+        }
+    
+        return head->next;
+    }
+};
+
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
