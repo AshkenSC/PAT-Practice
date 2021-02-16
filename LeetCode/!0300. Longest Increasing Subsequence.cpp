e53dbcba48a006a0df234ab91c5f786c9cbaf40b
ashken <393940378@qq.com>
2021-02-16 10:20:59 +0800

    LeetCode 0300. Longest Increasing Subsequence
    重做本题。

--- LeetCode/!0300. Longest Increasing Subsequence.cpp
+++ LeetCode/!0300. Longest Increasing Subsequence.cpp
@@ -20,14 +20,15 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
         int res = 1;
-        int len = nums.size();
-        vector<int> dp(len, 1);
 
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] < dp[i])
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
                     dp[i] = max(dp[i], 1 + dp[j]);
+                }
             }
             res = max(res, dp[i]);
         }
@@ -35,5 +36,4 @@ public:
         return res;
     }
 };
-
 
