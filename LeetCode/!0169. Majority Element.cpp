17915d82fae50a55f2a1ec705883b63c00e690f9
ashken <393940378@qq.com>
2021-02-18 02:16:50 +0800

    LeetCode 0169. Majority Element
    重做本题。

--- LeetCode/!0169. Majority Element.cpp
+++ LeetCode/!0169. Majority Element.cpp
@@ -11,19 +11,23 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int numCount = 1;
-        int currentValue = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == currentValue) {
-                numCount++;
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == candidate) {
+                ++count;
             }
             else {
-                numCount--;
-                if (numCount == 0) {
-                    currentValue = nums[i + 1];
-                }
+                --count;
+            }
+
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
             }
         }
-        return currentValue;
+
+        return candidate;
     }
 };
