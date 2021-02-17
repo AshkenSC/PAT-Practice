3fad104e7671bfc117fadcd89333defaf82fc756
ashken <393940378@qq.com>
2021-02-18 01:17:55 +0800

    LeetCode 0062. Unique Paths.cpp
    重做本题。

--- LeetCode/0062. Unique Paths.cpp
+++ LeetCode/0062. Unique Paths.cpp
@@ -32,3 +32,30 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+
+// 二刷，代码有些冗余
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i != 0 && j != 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+                else if (i == 0 && j != 0) {
+                    dp[i][j] = dp[i][j - 1];
+                }
+                else if (i != 0 && j == 0) {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                else {
+                    dp[i][j] = 1;
+                }
+            }
+        }
+    
+        return dp[m - 1][n - 1];
+    }
+};
