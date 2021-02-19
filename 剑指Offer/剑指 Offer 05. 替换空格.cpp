b657fdcd34e9a3224b73348a3d542b7b3a441d5e
ashken <393940378@qq.com>
2021-02-20 00:19:31 +0800

    剑指 Offer 05. 替换空格
    请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
    
    思路：逐个字符检查并保存，遇到空格就替换为保存%20。

---
+++ 剑指Offer/剑指 Offer 05. 替换空格.cpp
@@ -0,0 +1,26 @@
+/*
+剑指 Offer 05. 替换空格
+
+请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+
+思路：逐个字符检查并保存，遇到空格就替换为保存%20。
+
+*/
+
+class Solution {
+public:
+    string replaceSpace(string s) {
+        string res;
+        for (const char &c : s) {
+            if (c == ' ') {
+                res.push_back('%');
+                res.push_back('2');
+                res.push_back('0');
+            }
+            else {
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
