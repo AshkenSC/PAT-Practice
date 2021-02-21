23041200c1af4b7e695e5d35ea31b6785ed7e9fc
ashken <393940378@qq.com>
2021-02-21 20:18:26 +0800

    剑指 Offer 14- I. 剪绳子
    思路：动态规划。核心是搞清楚状态转移方程：
    当 i≥2 时，假设对正整数 i 拆分出的第一个正整数是 j（1≤j<i），则有以下两种方案：
    将 i 拆分成 j 和 i−j 的和，且 i−j 不再拆分成多个正整数，此时的乘积是 j×(i−j)；
    将 i 拆分成 j 和 i−j 的和，且 i−j 继续拆分成多个正整数，此时的乘积是 j×dp[i−j]。

---
+++ 剑指Offer/!剑指 Offer 14- I. 剪绳子.cpp
@@ -0,0 +1,31 @@
+/*
+剑指 Offer 14- I. 剪绳子
+
+给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？
+
+思路：动态规划。核心是搞清楚状态转移方程：
+当 i≥2 时，假设对正整数 i 拆分出的第一个正整数是 j（1≤j<i），则有以下两种方案：
+将 i 拆分成 j 和 i−j 的和，且 i−j 不再拆分成多个正整数，此时的乘积是 j×(i−j)；
+将 i 拆分成 j 和 i−j 的和，且 i−j 继续拆分成多个正整数，此时的乘积是 j×dp[i−j]。
+
+链接：https://leetcode-cn.com/problems/jian-sheng-zi-lcof/solution/dong-tai-gui-hua-shu-xue-by-sophia_fez/
+
+*/
+
+class Solution {
+public:
+    int cuttingRope(int n) {
+        vector<int> dp(n + 1);
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j < i; ++j) {
+                // 注意这里是求dp[i], j * (i - j), j * dp[i - j]三者最大值
+                // 别忘了带上dp[i]，因为在内层循环中，要多次计算出当前j值下的dp[i]，要选出其中最大值
+                dp[i] = max(j * (i - j), dp[i]);
+                dp[i] = max(dp[i], j * dp[i - j]);
+            }
+        }
+
+        return dp[n];
+    }
+};
