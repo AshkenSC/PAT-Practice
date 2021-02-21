26f10343228afb5522b45599efe497201ff4c790
ashken <393940378@qq.com>
2021-02-21 15:13:29 +0800

    剑指 Offer 13. 机器人的运动范围
    思路：BFS。关键是要知道，虽然说走四个方向，但实际只需要考虑向右和向下。

---
+++ 剑指Offer/!!剑指 Offer 13. 机器人的运动范围.cpp
@@ -0,0 +1,58 @@
+/*
+剑指 Offer 13. 机器人的运动范围
+
+地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。
+一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），
+也不能进入行坐标和列坐标的数位之和大于k的格子。
+例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。
+但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+
+思路：BFS。关键是要知道，虽然说走四个方向，但实际只需要考虑向右和向下。
+
+参考：
+https://leetcode-cn.com/problems/ji-qi-ren-de-yun-dong-fan-wei-lcof/solution/jian-zhi-offerer-shua-javadfs-bfs-tu-jie-py05/
+*/
+
+class Solution {
+public:
+    int movingCount(int m, int n, int k) {
+        int res = 0;
+
+        // 记录访问过的路径
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+        // 创建一个队列,用于BFS
+        queue<pair<int, int>> q;
+        q.emplace(pair<int, int>(0, 0));
+        while (q.empty() == false) {
+            // 队头出列
+            pair<int, int> current = q.front();
+            q.pop();
+            int x = current.first, y = current.second;
+
+            // 检查队头是否合法，若非法则跳过
+            if (x >= m || y >= n || digitSum(x) + digitSum(y) > k || visited[x][y]) {
+                continue;
+            }
+
+            // 若合法，则增加计数，记录访问，并向右向下做BFS
+            ++res;
+            visited[x][y] = true;
+            // 向下走
+            q.emplace(pair<int, int>(x + 1, y));
+            // 向右走
+            q.emplace(pair<int, int>(x, y + 1));
+        }
+
+        return res;
+    }
+
+    int digitSum(int num) {
+        int sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+        return sum;
+    }
+};
