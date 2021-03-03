1c55661fdc6a1559a61643a2744543b138d83dbd
ashken <393940378@qq.com>
2021-03-04 04:34:50 +0800

    0215. Kth Largest Element in an Array
    重做本题 。

--- LeetCode/!!0215. Kth Largest Element in an Array.cpp
+++ LeetCode/!!0215. Kth Largest Element in an Array.cpp
@@ -36,36 +36,41 @@ public:
 class Solution {
 public:
     void maxHeapify(vector<int>& a, int i, int heapSize) {
-        int left = i * 2 + 1, right = i * 2 + 2;
-        int largest = i;
-        if (left < heapSize && a[left] > a[largest]) {
-            largest = left;
-        }
-        if (right < heapSize && a[right] > a[largest]) {
-            largest = right;
-        }
-        if (largest != i) {
-            swap(a[largest], a[i]);
-            maxHeapify(a, i, heapSize);
+       int left = 2 * i + 1, right = 2 * i + 2, largest = i;
+
+       if (left < heapSize && a[left] > a[largest]) {
+           largest = left;
+       }
+       if (right < heapSize && a[right] > a[largest]) {
+           largest = right;
+       }
+
+       if (largest != i) {
+           swap(a[largest], a[i]);
+           maxHeapify(a, largest, heapSize);
+       }
+    }
+
+    void buildMaxHeap(vector<int>& nums, int heapSize) {
+        for (int i = heapSize / 2; i >= 0; --i) {
+            maxHeapify(nums, i, heapSize);
         }
     }
 
-    void buildMaxHeap(vector<int>& a, int heapSize) {
-        for (int i = heapSize / 2; i >= 0; --i) {
-            maxHeapify(a, i, heapSize);
-        }
-    }
-    
     int findKthLargest(vector<int>& nums, int k) {
         int heapSize = nums.size();
         buildMaxHeap(nums, heapSize);
-        for (int i = nums.size() - 1; i >= nums.size() - k + 1; --i) {
+
+        for (int i = nums.size() - 1; i > nums.size() - k; --i) {
             swap(nums[0], nums[i]);
-            maxHeapify(nums, 0, --heapSize);
+            --heapSize;
+            maxHeapify(nums, 0, heapSize);
         }
+
         return nums[0];
     }
 };
+
 
 int main() {
     Solution sol;
