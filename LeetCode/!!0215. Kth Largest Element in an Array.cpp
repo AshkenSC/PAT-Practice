2cf0c211b37a362d5b5ba04f23dba145647b6220
ashken <393940378@qq.com>
2021-03-17 14:28:16 +0800

    LeetCode 0215. Kth Largest Element in an Array
    更新小根堆排序法。

--- LeetCode/!!0215. Kth Largest Element in an Array.cpp
+++ LeetCode/!!0215. Kth Largest Element in an Array.cpp
@@ -8,31 +8,64 @@
 
 思路2：
 大根堆排序法。首先将整个树完整构造为一个大根堆，然后依次弹出根节点k次（弹出也就是交换到树末尾并不再访问），
-注意每一次弹出（交换根节点，缩小堆范围）都要重新建立堆。
+注意每一次弹出（交换根节点，缩小堆范围）都要重新调整堆。
+
+思路3：
+小根堆排序法。维护一个大小为k的小根堆。
+首先在原数组从0到k-1构建一个小根堆。然后从k到数组尾巴，与堆顶比较。一旦比堆顶大，就交换，调整堆。
+最后堆顶就是第k大的数。
 */
 
 #include <vector>
 #include <iostream>
 using namespace std;
 
-class Solution1 {
+
+// 小根堆
+class Solution {
+private:
+    void heapify(vector<int>& nums, int i, int heapSize) {
+        int left = i * 2 + 1, right = i * 2 + 2;
+        int currentMin = i;
+
+        if (left < heapSize && nums[left] < nums[currentMin]) {
+            currentMin = left;
+        }
+        if (right < heapSize && nums[right] < nums[currentMin]) {
+            currentMin = right;
+        }
+
+        if (currentMin != i) {
+            swap(nums[currentMin], nums[i]);
+            heapify(nums, currentMin, heapSize);
+        }
+    }
+
+    void buildHeap(vector<int>& nums, int heapSize) {
+        for (int i = heapSize / 2; i >= 0; --i) {
+            heapify(nums, i, heapSize);
+        }
+    }
+
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int temp;
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < nums.size() - i - 1; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }   
+        // 建立一个k大小的小根堆
+        buildHeap(nums, k);
+        // 遍历nums中第k + 1以后的数，如果比堆顶大，则交换，并调整堆
+        for (int i = k; i < nums.size(); ++i) {
+            if (nums[i] > nums[0]) {
+                swap(nums[i], nums[0]);
+                // 注意这里的heapSize，不能写nums.size()
+                heapify(nums, 0, k);
             }
         }
 
-        return nums[nums.size() - k];
+        // 输出第k大的数
+        return nums[0];
     }
 };
 
+// 大根堆
 class Solution {
 public:
     void maxHeapify(vector<int>& a, int i, int heapSize) {
@@ -68,6 +101,25 @@ public:
         }
 
         return nums[0];
+    }
+};
+
+// 冒泡排序
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int temp;
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < nums.size() - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }   
+            }
+        }
+
+        return nums[nums.size() - k];
     }
 };
 
--- Playground/main.cpp
+++ Playground/main.cpp
@@ -35,37 +35,51 @@ dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + a[i][j]
 */
 
 class Solution {
+private:
+    int res;
+
+    int partition(vector<int>& a, int low, int high) {
+        int i = low, j = high + 1, pivot = a[low];
+
+        while (true) {
+            while (a[++i] > pivot) {
+                if (i >= high) break;
+            }
+            while (a[--j] < pivot) {
+                if (j <= low) break;
+            }
+
+            if (i >= j) break;
+        
+            swap(a[i], a[j]);
+        }
+        swap(a[low], a[j]);
+
+        return j;
+    }
+
+    void partitionArray(vector<int>& a, int low, int high, int k) {
+        int m = partition(a, low, high);
+
+        if (m == k - 1) {
+            res = a[m];
+            return;
+        }
+        else if (k - 1 < m) {
+            partitionArray(a, low, m - 1, k);
+        }
+        else if (k - 1 > m) {
+            partitionArray(a, m + 1, high, k);
+        }
+    }
+
+
 public:
-    vector<int> singleNumbers(vector<int>& nums) {
-        // 首先，将nums中的所有数进行亦或。
-        // 最终得到的ret，出现两次的数亦或得全0，两个单独的数互不相同的比特位在ret中为1
-        int ret = 0;
-        for (int n : nums) {
-            ret ^= n;
-        }
-
-        // 找到ret中为1的最低位，也就是两个单独的数最低的不同比特位
-        int div = 1;
-        while ((div & ret) == 0) {
-            div <<= 1;
-        }
-
-        // 利用上一步找到的最低不同比特位div，进行分组
-        // 和div做亦或操作，等于1，放到a组，等于0，放到b组
-        // 出现两次的数由于在该位相同，会被分到同一组，从而消去
-        // 出现一次的两个数由于在该位不同，所以必定会分到不同组
-        int a = 0, b = 0;
-        for (int n : nums) {
-            if (div & n)
-                a ^= n;
-            else
-                b ^= n;
-        }
-
-        return vector<int>{a, b};
+    int findKthLargest(vector<int>& nums, int k) {
+        partitionArray(nums, 0, nums.size() - 1, k);
+        return res;
     }
 };
-
 
 struct TreeNode
 {
@@ -87,8 +101,8 @@ int main() {
     // [[1,3,1],[1,5,1],[4,2,1]]
     Solution sol;
     vector<vector<int>> input({{1, 3, 1}, {1, 5, 1}, {4, 2, 1}});
-    vector<int> test({0,1,2,3,4,5,6});
-    int n = sol.missingNumber(test);
+    vector<int> test({1});
+    int n = sol.findKthLargest(test, 1);
     cout << n;
 
     return 0;
