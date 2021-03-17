69731ca0edff464230f6c8a91e0bd50e14450ef6
ashken <393940378@qq.com>
2021-03-17 10:44:47 +0800

    剑指 Offer 56 - I. 数组中数字出现的次数

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -34,23 +34,35 @@ dp[0][0] = a[0][0]
 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + a[i][j]
 */
 
-
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int left = 0, right = nums.size();
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == mid) {
-                left = mid + 1;
-            }
-            else if (nums[mid] != mid) {
-                right = mid;
-            }
+    vector<int> singleNumbers(vector<int>& nums) {
+        // 首先，将nums中的所有数进行亦或。
+        // 最终得到的ret，出现两次的数亦或得全0，两个单独的数互不相同的比特位在ret中为1
+        int ret = 0;
+        for (int n : nums) {
+            ret ^= n;
         }
 
-        return left;
+        // 找到ret中为1的最低位，也就是两个单独的数最低的不同比特位
+        int div = 1;
+        while ((div & ret) == 0) {
+            div <<= 1;
+        }
+
+        // 利用上一步找到的最低不同比特位div，进行分组
+        // 和div做亦或操作，等于1，放到a组，等于0，放到b组
+        // 出现两次的数由于在该位相同，会被分到同一组，从而消去
+        // 出现一次的两个数由于在该位不同，所以必定会分到不同组
+        int a = 0, b = 0;
+        for (int n : nums) {
+            if (div & n)
+                a ^= n;
+            else
+                b ^= n;
+        }
+
+        return vector<int>{a, b};
     }
 };
 
