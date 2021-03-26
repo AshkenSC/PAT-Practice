61817b7b4be174e556f0287769c2c256909846c8
ashken <393940378@qq.com>
2021-03-26 23:58:03 +0800

    剑指 Offer 60. n个骰子的点数
    把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s的所有可能的值出现的概率。
    你需要用一个浮点数数组返回答案，其中第 i 个元素代表这 n 个骰子所能掷出的点数集合中第 i 小的那个的概率。
    
    思路：
    使用动态规划，并进行空间优化。

---
+++ 剑指Offer/!!剑指 Offer 60. n个骰子的点数.cpp
@@ -0,0 +1,29 @@
+/*
+剑指 Offer 60. n个骰子的点数
+
+把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s的所有可能的值出现的概率。
+你需要用一个浮点数数组返回答案，其中第 i 个元素代表这 n 个骰子所能掷出的点数集合中第 i 小的那个的概率。
+
+思路：
+使用动态规划，并进行空间优化。
+
+参考：
+https://leetcode-cn.com/problems/nge-tou-zi-de-dian-shu-lcof/solution/jian-zhi-offer-60-n-ge-tou-zi-de-dian-sh-z36d/
+*/
+
+class Solution {
+public:
+    vector<double> dicesProbability(int n) {
+        vector<double> dp(6, 1.0 / 6.0);
+        for (int i = 2; i <= n; i++) {
+            vector<double> tmp(5 * i + 1, 0);
+            for (int j = 0; j < dp.size(); j++) {
+                for (int k = 0; k < 6; k++) {
+                    tmp[j + k] += dp[j] / 6.0;
+                }
+            }
+            dp = tmp;
+        }
+        return dp;
+    }
+};
