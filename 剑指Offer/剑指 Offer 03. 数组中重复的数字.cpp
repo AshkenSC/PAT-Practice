745efef14a0e06d14202a451b1cfe94fd2727de8
ashken <393940378@qq.com>
2021-04-11 10:03:45 +0800

    剑指 Offer 03. 数组中重复的数字
    重做本题。

--- 剑指Offer/剑指 Offer 03. 数组中重复的数字.cpp
+++ 剑指Offer/剑指 Offer 03. 数组中重复的数字.cpp
@@ -4,21 +4,19 @@
 思路：原地哈希。
 参考答案：
 https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/solution/yuan-di-zhi-huan-shi-jian-kong-jian-100-by-derrick/
-
 */
 
 class Solution {
 public:
     int findRepeatNumber(vector<int>& nums) {
         for (int i = 0; i < nums.size(); ++i) {
-            while (nums[i] != i) {
+            while (i != nums[i]) {
                 if (nums[i] == nums[nums[i]]) {
                     return nums[i];
                 }
-                swap(nums[i], nums[nums[i]]); 
+                swap(nums[i], nums[nums[i]]);
             }
         }
-
         return -1;
     }
 };
