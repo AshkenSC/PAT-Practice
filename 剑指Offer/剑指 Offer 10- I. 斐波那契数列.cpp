ca5cb00993bc4de3ae84d26ebf430070a0b06253
ashken <393940378@qq.com>
2021-04-15 10:47:53 +0800

    剑指 Offer 10- I. 斐波那契数列
    重做本题。

--- 剑指Offer/剑指 Offer 10- I. 斐波那契数列.cpp
+++ 剑指Offer/剑指 Offer 10- I. 斐波那契数列.cpp
@@ -10,18 +10,21 @@
 class Solution {
 public:
     int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        
         vector<long long> f(3, 0);
         f[1] = 1; f[2] = 1;
-
-        if (n < 3) {
-            return f[n];
-        }
-
         for (int i = 3; i <= n; ++i) {
             f[0] = f[1];
             f[1] = f[2];
             f[2] = (f[0] + f[1]) % 1000000007;
         }
+        
         return f[2];
     }
 };
