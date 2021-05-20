6bd95495386b58ee251cc701f66b7c423af00fba
ashken <393940378@qq.com>
2021-05-21 00:00:36 +0800

    剑指 Offer 14- I. 剪绳子
    重做本题。

--- 剑指Offer/!剑指 Offer 14- I. 剪绳子.cpp
+++ 剑指Offer/!剑指 Offer 14- I. 剪绳子.cpp
@@ -9,7 +9,8 @@
 将 i 拆分成 j 和 i−j 的和，且 i−j 不再拆分成多个正整数，此时的乘积是 j×(i−j)；
 将 i 拆分成 j 和 i−j 的和，且 i−j 继续拆分成多个正整数，此时的乘积是 j×dp[i−j]。
 
-链接：https://leetcode-cn.com/problems/jian-sheng-zi-lcof/solution/dong-tai-gui-hua-shu-xue-by-sophia_fez/
+链接：
+https://leetcode-cn.com/problems/jian-sheng-zi-lcof/solution/dong-tai-gui-hua-shu-xue-by-sophia_fez/
 */
 
 class Solution {
