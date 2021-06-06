0ac4305c4118fba4da212b5aeebaa9b402c4e67d
ashken <393940378@qq.com>
2021-06-06 22:33:21 +0800

    剑指 Offer 65. 不用加减乘除做加法
    重做本题。

--- 剑指Offer/!!剑指 Offer 65. 不用加减乘除做加法.cpp
+++ 剑指Offer/!!剑指 Offer 65. 不用加减乘除做加法.cpp
@@ -8,7 +8,7 @@
 然后将这两部分的结果用xor进一步相加，并进一步用and计算进位情况；
 不断循环，直到第2）部分完全为0为止。
 
-注意，使用c++时，对于第2）部分，需要强制转为unsigned int进行循环左移。否则会报错。
+注意！使用c++时，对于第2）部分，需要强制转为unsigned int进行循环左移。否则会报错。
 
 参考：
 https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/solution/mian-shi-ti-65-bu-yong-jia-jian-cheng-chu-zuo-ji-7/
